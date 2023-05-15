fa5a08827121495d41af0d6ffad5b1daf2179ab6
yushicheng <1542874601@qq.com>
2023-05-16 00:41:47 +0800

    temp:临时节点标记 星期二 凌晨 12:41:47 五月 16日 2023 创建了 .npmrc 创建了 scripts/install.js 修改了 package.json

---
+++ scripts/install.js
@@ -0,0 +1,21 @@
+const { bootstrap } = require("global-agent");
+const { downloadArtifact } = require("@electron/get");
+
+(async () => {
+
+  console.log(process.env.http_proxy);
+  process.env.http_proxy && (process.env.GLOBAL_AGENT_HTTP_PROXY = process.env.http_proxy);
+  console.log(process.env.https_proxy);
+  process.env.https_proxy && (process.env.GLOBAL_AGENT_HTTPS_PROXY = process.env.https_proxy);
+  bootstrap();
+
+  const zipFilePath = await downloadArtifact({
+    version: '24.3.0',
+    platform: 'darwin',
+    artifactName: 'electron',
+    artifactSuffix: 'symbols',
+    arch: 'x64',
+  });
+
+  console.log("zipFilePath", zipFilePath);
+})();
