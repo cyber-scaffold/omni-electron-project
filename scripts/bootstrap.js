65985e03b6f98464399cbb154651da78bfe98020
frankleo <154284601@qq.com>
2023-05-22 10:07:36 +0800

    temp:临时节点标记 星期一 上午 10:07:36 五月 22日 2023 修改了 package.json 修改了 scripts/bootstrap.js

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -1,15 +1,27 @@
+const fs = require("fs");
 const path = require("path");
 const spawn = require("cross-spawn");
+const chokidar = require("chokidar");
 const { webpack } = require("webpack");
+const { promisify } = require("util");
 
-const assert_file = require("./transactions/assert_file");
 const webpack_application_dev_config = require("./webpack/development/webpack.application.dev.config");
 const webpack_preload_dev_config = require("./webpack/development/webpack.preload.dev.config");
 const webpack_process_dev_config = require("./webpack/development/webpack.process.dev.config");
 
 (async () => {
 
+  await promisify(fs.rm)(path.resolve(process.cwd(), "./dist/"), { recursive: true });
+
   const process_task_list = [];
+
+  const watcher = chokidar.watch([
+    path.resolve(process.cwd(), "./dist/process.js")
+  ], { persistent: true });
+  watcher.on("all", async () => {
+    process_task_list.forEach((single_process_task) => single_process_task.kill());
+    process_task_list.push(spawn("electron", [path.resolve(process.cwd(), "./dist/process.js")], { stdio: "inherit" }));
+  });
 
   /** 编译前端文件 **/
   const webpack_application_dev_compair = webpack(webpack_application_dev_config);
@@ -30,12 +42,6 @@ const webpack_process_dev_config = require("./webpack/development/webpack.proces
       console.log(error)
     } else {
       console.log(stats.toString({ colors: true }));
-      if (await assert_file()) {
-        process_task_list.forEach((single_process_task) => single_process_task.kill());
-        process_task_list.push(spawn("electron", [path.resolve(process.cwd(), "./dist/process.js")], { stdio: "inherit" }));
-      } else {
-        return false;
-      };
     };
   });
 
@@ -47,12 +53,8 @@ const webpack_process_dev_config = require("./webpack/development/webpack.proces
       console.log(error)
     } else {
       console.log(stats.toString({ colors: true }));
-      if (await assert_file()) {
-        process_task_list.forEach((single_process_task) => single_process_task.kill());
-        process_task_list.push(spawn("electron", [path.resolve(process.cwd(), "./dist/process.js")], { stdio: "inherit" }));
-      } else {
-        return false;
-      };
+      process_task_list.forEach((single_process_task) => single_process_task.kill());
+      process_task_list.push(spawn("electron", [path.resolve(process.cwd(), "./dist/process.js")], { stdio: "inherit" }));
     };
   });
 
